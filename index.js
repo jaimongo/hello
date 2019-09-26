3b4c48ea89f94c13b0366c3e247501a83738b22a
jaime <j.zero2000@gmail.com>
2019-09-26 17:30:06 -0500

    subir datos de ip a json

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-
-const express = require('express');
+const express= require('express');
 const app = express();
+var datos=[];
 
-//////////manejar peticiones
+var personas={
+	nombre:null,
+	edad:null,
+}
+
+
+
+
 app.get("/", (req, res) =>{
-	res.send("hola mundo");
+    console.log(req);
+    res.send("Hola mundo!");
 });
 
-/////////run server
-app.listen(3000, ()=>{
-	console.log("server is running....");
+
+app.get("/:id/:nombre/:edad", (req, res) =>{
+	if(req.url!="/favicon.ico"){
+    var dato = req.params.id;
+    console.log(dato);
+    datos.push(dato);
+    console.log(datos);	
+
+
+    var dato1 = req.params.nombre;
+    var dato2 = req.params.edad;
+
+    console.log(dato1);
+    console.log(dato2);
+
+    personas.nombre=dato1;
+    personas.edad=dato2;
+
+    res.send();
+
+    console.log(personas);	
+	}
+
+});
+
+
+app.listen(3000, () =>{
+    console.log("Server is running...");
 });
